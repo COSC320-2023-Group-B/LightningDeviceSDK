dcb8dd7841faeb14125feaeefc3a2d1bd02a64e6
Josh Edwards <josh@terasoft.com.au>
2023-08-15 17:12:50 +1000

    Add test plugin

---
+++ development/devices/Caretaker4/Caretaker4.ts
@@ -0,0 +1,13 @@
+export class DeviceClass {
+
+    constructor() {
+        console.log('In the constructor');
+    }
+
+}
+
+module.exports = {
+    getDeviceClasses() {
+       return [new DeviceClass()];
+    }
+};
